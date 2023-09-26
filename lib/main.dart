10cde5603fbeaf8f05ea70ab012972de68781923
felix-gustavo <gustavovieira1945@gmail.com>
2023-09-25 23:45:05 -0300

    config .env

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,27 @@ import 'package:flutter/material.dart';
 import 'package:form_validator/form_validator.dart';
 
 import 'app/app.dart';
-import 'firebase_options.dart';
 
 void main() async {
   await runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
+
+    const apiKey = String.fromEnvironment('API_KEY');
+    const appId = String.fromEnvironment('APP_ID');
+    const messagingSenderId = String.fromEnvironment('MESSAGING_SENDER_ID');
+    const projectId = String.fromEnvironment('PROJECT_ID');
+    const authDomain = String.fromEnvironment('AUTH_DOMAIN');
+    const storageBucket = String.fromEnvironment('STORAGE_BUCKET');
+
     await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+      options: const FirebaseOptions(
+        apiKey: apiKey,
+        appId: appId,
+        messagingSenderId: messagingSenderId,
+        projectId: projectId,
+        authDomain: authDomain,
+        storageBucket: storageBucket,
+      ),
     );
 
     ValidationBuilder.setLocale('pt-br');
